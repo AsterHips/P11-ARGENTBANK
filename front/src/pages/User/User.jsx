a28948796908f558506610ae5dd66b876e766b54
Charles Burnatowski <charles.burna@hotmail.fr>
2023-11-07 22:49:07 +0100

    ajout de la condition

--- front/src/pages/User/User.jsx
+++ front/src/pages/User/User.jsx
@@ -1,4 +1,6 @@
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router";
+import { useEffect } from "react";
 
 import Welcome from "../../components/Welcome/Welcome";
 import Transactions from "../../components/Transactions/Transactions";
@@ -7,6 +9,14 @@ import Edit from "../../components/Edit/Edit";
 function User() {
     const user = useSelector((store) => store.user);
     const edit = user.editMode;
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!user.token) {
+            navigate("/signin");
+        }
+    },[user.token, navigate]);
 
     return (
         <main className={`main ${edit ? "" : "bg-dark"}`}>
